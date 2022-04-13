5e65285fcc1c91e6e8582f19db11f86081fd06c8
bycho91 <bycho1991@gmail.com>
2022-04-12 22:10:00 -0500

    romanize to numerals

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("test 24");
+function romanize(num) {
+  let romanized = "";
+  let numeralsDict = {
+    1: "I",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M",
+  };
+
+  Object.keys(numeralsDict)
+    .reverse()
+    .forEach((key) => {
+      while (num >= key) {
+        romanized += numeralsDict[key];
+        num = num - key;
+      }
+    });
+
+  return romanized;
+}
+
+console.log(romanize(3333));
