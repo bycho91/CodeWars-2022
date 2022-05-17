5b064950c46c82cb661e4dfe8e01dc6d519694ff
bycho91 <bycho1991@gmail.com>
2022-05-17 12:03:44 -0500

    romanize updated

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ function romanize(num) {
   let romanized = "";
   let numeralsDict = {
     1: "I",
+    4: "IV",
     5: "V",
+    9: "IX",
     10: "X",
+    40: "XL",
     50: "L",
+    90: "XC",
     100: "C",
+    400: "CD",
     500: "D",
+    900: "CM",
     1000: "M",
   };
 
@@ -22,4 +28,4 @@ function romanize(num) {
   return romanized;
 }
 
-console.log(romanize(3333));
+console.log(romanize(1990));
