f7c0792b9c27c805abc4bcabf5d452a2e1e93c1b
bycho91 <bycho1991@gmail.com>
2022-05-22 14:20:00 -0500

    two sum test

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-function setBase(x) {
-  return function (y) {
-    return x + y;
-  };
-}
+var twoSum = function (nums, target) {
+  for (let i = 0; i <= nums.length - 1; i++) {
+    let x = target - nums[i];
+    for (let j = i + 1; j <= nums.length - 1; j++) {
+      if (x === nums[j]) {
+        return i, j;
+      }
+    }
+  }
 
-const addFive = setBase(5);
-
-const addTen = setBase(10);
-
-console.log(addFive(3));
-
-console.log(addTen(5));
+  return "no values";
+};
